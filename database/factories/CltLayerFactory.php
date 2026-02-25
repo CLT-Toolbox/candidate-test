fae67b774e319d42b5c27f8308ebb7a187435d82
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2026-02-25 21:13:16 +0700

    add model'sf factories

---
+++ database/factories/CltLayerFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\CltLayup;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CltLayer>
+ */
+class CltLayerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'layup_id' => CltLayup::factory(),
+            'layer_order' => fake()->numberBetween(1, 10),
+            'thickness' => fake()->randomFloat(2),
+            'width' => fake()->randomFloat(2),
+            'angle' => fake()->randomFloat(2, 0, 360),
+        ];
+    }
+}
