e0a7b9aeb9b6c32fe33035a1a94a233af10dd5dd
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2026-02-25 21:28:19 +0700

    fix primary button

--- resources/views/supplier/index.blade.php
+++ resources/views/supplier/index.blade.php
@@ -70,11 +70,9 @@
                                     </td>
                                     <td class="px-6 py-4">
                                         <div class="flex justify-end items-center space-x-2">
-                                            <a href="{{ route('suppliers.layups.index', $supplier->id) }}">
-                                                <x-primary-button>
-                                                    {{ __('Layups') }}
-                                                </x-primary-button>
-                                            </a>
+                                            <x-primary-button href="{{ route('suppliers.layups.index', $supplier->id) }}">
+                                                {{ __('Layups') }}
+                                            </x-primary-button>
                                             <x-secondary-button x-data="" x-on:click.prevent="$dispatch('open-modal', 'edit-supplier-modal-{{ $supplier->id }}')" class="text-blue-600 dark:text-blue-500 hover:text-blue-900 dark:hover:text-blue-400" title="Edit">
                                                 <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path>
