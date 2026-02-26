628af8a9c6028e9704b4d5248170c35f37326ef5
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2026-02-26 11:02:32 +0700

    fix ui in layers index

--- resources/views/supplier/layups/layers/index.blade.php
+++ resources/views/supplier/layups/layers/index.blade.php
@@ -14,12 +14,10 @@
                             {{ __('Back to Layups') }}
                         </a>
                     </div>
-                    <div class="flex justify-between items-center mb-6">
-                        <div class="">
-                            <h2 class="text-2xl font-semibold text-gray-800 dark:text-gray-200 pb-2">Layers List</h2>
-                            <p>Supplier : {{ $supplier->name }}</p>
-                            <p>Layup : {{ $layup->name }}</p>
-                        </div>
+                    <h2 class="text-2xl font-semibold text-gray-800 dark:text-gray-200 pb-2">Layers List</h2>
+                    <p>Supplier : {{ $supplier->name }}</p>
+                    <p>Layup : {{ $layup->name }}</p>
+                    <div class="flex justify-end items-center mt-4">
                         <div class="flex space-x-2">
                             <x-primary-button x-data="" x-on:click.prevent="$dispatch('open-modal', 'add-layer-modal')">
                                 <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
