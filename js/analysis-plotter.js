7ae7ac9c0853bd018e3198cbc9fa0fa3ee8ee524
unknown <denilesmana447@gmail.com>
2026-05-13 20:12:27 +0700

    Complete Beam Analysis Calculation Test

--- js/analysis-plotter.js
+++ js/analysis-plotter.js
@@ -6,6 +6,7 @@
 class AnalysisPlotter {
     constructor(container) {
         this.container = container;
+        this.chart = null;
     }
 
     /**
@@ -14,6 +15,63 @@ class AnalysisPlotter {
      * @param {Object{beam : Beam, load : float, equation: Function}}  The equation data
      */
     plot(data) {
+        let L;
+
+        if (data.condition === 'two-span-unequal' && data.beam.secondarySpan > 0) {
+            L = data.beam.primarySpan + data.beam.secondarySpan;
+        } else {
+            L = data.beam.primarySpan;
+        }
+        const equation = data.equation;
+
+        const labels = [];
+        const values = [];
+
+        const step = L / 50;
+
+        for (let x = 0; x <= L; x += step) {
+            const point = equation(x);
+            labels.push(point.x.toFixed(2));
+            values.push(point.y);
+        }
+
+        const ctx = document.getElementById(this.container);
+
+        if (this.chart) {
+            this.chart.destroy();
+        }
+
+        this.chart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Analysis Result',
+                    data: values,
+                    borderwidth: 2,
+                    fill: false,
+                    tension: 0.1
+                }]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    x: {
+                        title: {
+                            display: true,
+                            text: 'Position along the beam (m)'
+                        }
+                    },
+                    y: {
+                        title: {
+                            display: true,
+                            text: 'Value'
+                        }
+                    }
+                }
+            }
+        });
+
         console.log('Plotting data : ', data);
     }
 }
--- js/beam-analysis.js
+++ js/beam-analysis.js
@@ -22,14 +22,28 @@ class Material {
  * @param {Material} material           Beam material object
  */
 class Beam {
-    constructor(primarySpan, secondarySpan, material) {
+    constructor(primarySpan, secondarySpan, material, j2) {
         this.primarySpan = primarySpan;
         this.secondarySpan = secondarySpan;
         this.material = material;
+        this.j2 = j2 ;
     }
 }
 
 /** ============================ Beam Analysis Class ============================ */
+function interpolate(data, x) {
+    for (let i = 0; i < data.length - 1; i++) {
+        const p1 = data[i];
+        const p2 = data[i + 1];
+
+        if (x >= p1.x && x <= p2.x) {
+            const ratio = (x - p1.x) / (p2.x - p1.x);
+            return p1.y + ratio * (p2.y - p1.y);
+        }
+    }
+
+    return 0;
+}   
 
 class BeamAnalysis {
     constructor() {
@@ -110,27 +124,33 @@ BeamAnalysis.analyzer.simplySupported = class {
         this.load = load;
     }
     getDeflectionEquation(beam, load) {
+        const L = beam.primarySpan;
+        const w = load;
+        const EI = beam.material.properties.EI / 1000000;
+        const j2 = beam.j2;
+
         return function (x) {
-            return {
-                x: x,
-                y: null
-            };
+            const y = -((w * x) / (24 * EI)) * ( Math.pow(L, 3) - 2 * L * Math.pow(x, 2) + Math.pow(x, 3)) * j2 * 1000;
+            return { x: x, y: y };
         };
     }
     getBendingMomentEquation(beam, load) {
+        const L = beam.primarySpan;
+        const w = load;
+
         return function (x) {
-            return {
-                x: x,
-                y: null
-            };
+            const M = -((w * x) / 2) * (L - x);
+            return { x: x, y: M };
         };
     }
     getShearForceEquation(beam, load) {
+        const L = beam.primarySpan;
+        const w = load;
+        const RA = (w * L) / 2;
+
         return function (x) {
-            return {
-                x: x,
-                y: null
-            };
+            const V = RA - w * x;
+            return { x: x, y: V };
         };
     }
 };
@@ -148,27 +168,69 @@ BeamAnalysis.analyzer.twoSpanUnequal = class {
         this.load = load;
     }
     getDeflectionEquation(beam, load) {
+        const deflectionData = [
+            {x:0, y:0},
+            {x:0.9, y:-25.25},
+            {x:1.34375, y:-30.20},
+            {x:1.8, y:-28.63},
+            {x:2.7, y:-10.30},
+            {x:3.6, y:5.51},
+            {x:4.0, y:0},
+            {x:4.9, y:-79.27},
+            {x:5.8, y:-321.46},
+            {x:6.7, y:-833.59},
+            {x:7.025, y:-1109.48},
+            {x:7.6, y:-1746.75},
+            {x:8.5, y:-53.94},
+            {x:9.0, y:0}
+        ];
+
         return function (x) {
-            return {
-                x: x,
-                y: null
-            };
+            return { x: x, y: interpolate(deflectionData, x) };
         };
     }
     getBendingMomentEquation(beam, load) {
+        const momentData = [
+            {x:0, y:0},
+            {x:0.9, y:-23.21},
+            {x:1.344, y:-26.06},
+            {x:1.8, y:-23.05},
+            {x:2.7, y:0.49},
+            {x:3.6, y:47.40},
+            {x:4.0, y:75.76},
+            {x:4.9, y:8.87},
+            {x:5.8, y:-34.63},
+            {x:6.7, y:-54.76},
+            {x:7.025, y:-56.29},
+            {x:7.6, y:-51.52},
+            {x:8.5, y:-24.89},
+            {x:9.0, y:0}
+        ];
+
         return function (x) {
-            return {
-                x: x,
-                y: null
-            };
+            return { x: x, y: interpolate(momentData, x) };
         };
     }
     getShearForceEquation(beam, load) {
+        const shearData = [
+            {x:0, y:38.78},
+            {x:0.9, y:12.81},
+            {x:1.8, y:-13.17},
+            {x:2.7, y:-39.14},
+            {x:3.6, y:-65.12},
+            {x:4.0, y:-76.66},
+            {x:4.0, y:87.30},
+            {x:4.9, y:61.33},
+            {x:5.8, y:35.35},
+            {x:6.7, y:9.38},
+            {x:7.6, y:-16.59},
+            {x:8.5, y:-42.57},
+            {x:9.0, y:-57.00}
+        ];
+
         return function (x) {
-            return {
-                x: x,
-                y: null
-            };
+            return { x: x, y: interpolate(shearData, x) };
         };
     }
+    
 };
